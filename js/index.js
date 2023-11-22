d679140bb7a9de89db41f4c0d5d7c9cf68e39874
larissamc1397 <larissamc13@gmail.com>
2023-11-21 23:16:14 -0600

    modified index.html by adding form and added interactivity in the index.js

--- js/index.js
+++ js/index.js
@@ -13,3 +13,29 @@ for (var i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+var messageForm = document.forms.leave_message;
+messageForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+var usersName = event.target.usersName.value;
+var usersEmail = event.target.usersEmail.value;
+var usersMessage = event.target.usersMessage.value;
+console.log('Name:', usersName);
+console.log('Email:', usersEmail);
+console.log('Message:', usersMessage);
+
+var messageSection = document.getElementById('messages');
+var messageList = messageSection.querySelector('ul');
+var newMessage = document.createElement('li');
+newMessage.innerHTML = '<a href="mailto:' + usersEmail + '">' + usersName + '</a>' +
+'<span>' + usersMessage + '</span>';
+var removeButton = document.createElement('button');
+removeButton.innerText = 'remove';
+removeButton.type = 'button';
+removeButton.addEventListener('click', function() {
+  var entry = removeButton.parentNode;
+  entry.remove();
+});
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage);
+messageForm.reset();
+});
